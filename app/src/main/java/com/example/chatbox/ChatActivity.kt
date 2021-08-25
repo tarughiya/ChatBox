5f0eff156755c78f3f0a0f8d63b6857173d3e1b9
Taru <taru.ghiya@hub361.com>
2021-08-25 16:50:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chatbox/ChatActivity.kt
@@ -0,0 +1,27 @@
+package com.example.chatbox
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.example.chatbox.databinding.ActivityChatBinding
+
+
+class ChatActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityChatBinding
+    private val database = FirebaseDatabase.getInstance("https://chattrbox-7afac-default-rtdb.asia-southeast1.firebasedatabase.app").reference
+
+    val auth = FirebaseAuth.getInstance()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        val user = auth.currentUser!!
+        val fbUser = User(user.providerData[0].uid, "johnDoe")
+
+        database.child("users").child(user.providerData[0].uid).setValue(fbUser)
+    }
+}
---
+++ app/src/main/java/com/example/chatbox/User.kt
@@ -0,0 +1,6 @@
+package com.example.chatbox
+
+data class User(
+    val userId: String,
+    val userName: String
+)
